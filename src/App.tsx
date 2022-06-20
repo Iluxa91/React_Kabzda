7798f566309f89abef4aca1cc8ff1091c3f77699
Ilua Kisialiou <iluxa91@inbox.ru>
2022-06-20 14:27:31 +0300

    lesson25

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,10 @@ function App() {
     const [date, setDate] = useState(new Date())
 
     useEffect(() => {
+        console.log('effect')
 
         const intervalId = setInterval(() => {
+            console.log('interval')
             setDate(new Date)
 
         }, 1000)
@@ -33,3 +35,37 @@ function App() {
 }
 
 export default App;
+
+export const SetTimoutExample = () => {
+    const [text,setText] = useState('')
+
+    useEffect(()=>{
+        const timeoutId = setTimeout(()=>{
+            console.log('timeout')
+            setText('3seconds')
+        },3000)
+        return ()=>{
+            clearTimeout(timeoutId)
+        }
+    },[])
+    return <>
+        text:{text}
+    </>
+}
+ export const KeyPressInfo = () => {
+    const [text,setText] = useState('')
+     console.log('keypressRender')
+     useEffect(()=>{
+         const handler = (e:KeyboardEvent) => {
+             console.log(e.key)
+             setText(text+e.key)
+         }
+         window.addEventListener('keypress',handler)
+         return () => {
+            window.addEventListener('keypress',handler)
+         }
+     },[text])
+     return <>
+         text: {text}
+     </>
+ }
